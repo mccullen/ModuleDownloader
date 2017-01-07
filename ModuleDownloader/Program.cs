fdf9b2e2fa468a45a7c16df6b2b4e1b2aa8cddd0
mccullen <mccullenlc@gmail.com>
2017-01-07 10:04:36 -0500

    Add ModuleDownload() method to downloader, reaname ModuleDownloader.Core to WebUtilities, and install selenium webdriver.

--- ModuleDownloader/Program.cs
+++ ModuleDownloader/Program.cs
@@ -1,5 +1,6 @@
 ﻿/**
  * Name: Jeff McCullen
+ * Get Github extension then File -> Add to Source Control
  */
 using System;
 using System.Collections.Generic;
@@ -14,10 +15,16 @@ namespace ModuleDownloader
 	{
 		static void Main(string[] args)
 		{
-			 Downloader downloader = new Downloader();
-			string address = "http://vid20.pluralsight.com/clip-videos/scott-allen/c-sharp-fundamentals-with-visual-studio-2015-m0/c-sharp-fundamentals-with-visual-studio-2015-m0-01/1280x720mp4/20160721231755.mp4?OLUy-XYNM5nBL1y-aCQ7NeoKjY0ZUOX0wDMb_hT0rLkrsdKVG6UZUDfPT9O9JL5pmicDrVB30-BEkNg0G7loGoQmlKHNtNbkCSWQbki3FMfvC2_l1ebNyOxhNKHXLg0SA_CD-ew69T7b6nKwc4yBTo5rlOmhSW6I5FubsAXuo-poxc864agYHdkHZFUbJl6wwq0ztJbPOlM5FxiohN0naqMHVyeyZ4RkjXITY-jo2Bxh4o9lkaj9F_zKEVNHZbICZW1TbAox5wUlz6FCz6xm_iDHS6rVSBZt";
-			string filename = @"C:\Users\jeff\Videos\intro.mp4";
-			downloader.DownloadFile(address, filename);
+			DownloadModule();
+
+			Console.ReadLine();
+		}
+
+		private static void DownloadModule()
+		{
+			Downloader downloader = new Downloader();
+
+			downloader.DownloadModule();
 
 			Console.ReadLine();
 		}
---
+++ WebUtilities/Downloader.cs
@@ -0,0 +1,36 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+// Requires reference to WebDriver.Support.dll
+using OpenQA.Selenium.Support.UI;
+
+namespace WebUtilities
+{
+    public class Downloader
+    {
+		public void DownloadFile(string address, string filename)
+		{
+			using (WebClient client = new WebClient())
+			{
+				client.DownloadFile(address, filename);
+			}
+		}
+		public void DownloadModule()
+		{
+			Console.WriteLine("Please login and navigate to the module you want to download and press ENTER");
+			using (IWebDriver driver = new ChromeDriver())
+			{
+				driver.Navigate().GoToUrl("https://www.pluralsight.com/");
+				Console.ReadLine();
+			}
+		}
+    }
+}
